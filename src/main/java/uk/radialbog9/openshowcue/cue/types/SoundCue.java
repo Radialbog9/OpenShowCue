94b49d34344d9e82ce26183911bf6337ac1ac6c8
TheJoeCoder <joe@radialbog9.uk>
2023-03-15 21:38:43 +0000

    Add sound cue
    Signed-off-by: TheJoeCoder <joe@radialbog9.uk>

---
+++ src/main/java/uk/radialbog9/openshowcue/cue/types/SoundCue.java
@@ -0,0 +1,30 @@
+package uk.radialbog9.openshowcue.cue.types;
+
+import uk.radialbog9.openshowcue.cue.Cue;
+
+public class SoundCue implements Cue {
+    @Override
+    public void go() {
+
+    }
+
+    @Override
+    public void pause() {
+
+    }
+
+    @Override
+    public void resume() {
+
+    }
+
+    @Override
+    public void stop() {
+
+    }
+
+    @Override
+    public void next() {
+
+    }
+}
